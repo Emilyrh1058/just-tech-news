6c9066e66df4910e16e33281645922d89752c9f4
Emily Salinas <emilyrh1058@gmail.com>
2021-05-12 20:30:21 -0500

    added to comment-routes

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-
-// create associations
+// create model associations
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -40,5 +40,21 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote, Comment };
