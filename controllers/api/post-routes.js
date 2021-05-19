1eea3af7c0c528853954829b4520553912153397
Emily Salinas <emilyrh1058@gmail.com>
2021-05-19 00:33:42 -0500

    left off at 14.5.6

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
-const withAuth = require('../utils/auth');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get("/", (req, res) => {
@@ -158,4 +158,4 @@ router.put("/:id", withAuth, (req, res) => {
   }),
 
 
-  (module.exports = router);
+  module.exports = router;
