190bf65283ce66793955300e512fd36e62a9b016
Emily Salinas <emilyrh1058@gmail.com>
2021-05-20 00:51:50 -0500

    LA couldn't help with server error, redirecting to tutor

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -141,24 +141,25 @@ router.put("/:id", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 }),
-
-router.delete("/:id", withAuth, (req, res) => {
-  Post.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-      res.json(dbPostData);
+  router.delete("/:id", withAuth, (req, res) => {
+    console.log("id in router.delete", req.params.id);
+    Post.destroy({
+      where: {
+        id: req.params.id,
+      },
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-}),
-  
-  module.exports = router;
+      .then((dbPostData) => {
+        console.log('dbPostData', dbPostData);
+        if (!dbPostData) {
+          res.status(404).json({ message: "No post found with this id" });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch((err) => {
+        console.log('ERROR!!!')
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }),
+  (module.exports = router);
